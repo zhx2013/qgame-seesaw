308b54b592ff26d9c91c39aa56770e3dafdb8414
msf12 <msf12@hampshire.edu>
2013-12-07 09:47:50 -0500

    Update core.clj
    added :main true to :gen-class
    moved (native!) and the listener to the main so the only top-level functions are defs

--- src/qgame_seesaw/core.clj
+++ src/qgame_seesaw/core.clj
@@ -1,25 +1,23 @@
 (ns qgame-seesaw.core
-        (:use [seesaw.core])
-	(:gen-class))
+  (:gen-class :main true)
+  (:use [seesaw.core]))
 
-(native!)
+
 (def f (frame :title "QGAME" :width 800 :height 800))
 (defn display [content]
-        (config! f :content content)
-        content)
+	(config! f :content content)
+	content)
 (def area (text :multi-line? true :font "MONOSPACED-PLAIN-14"
-        :text ""))
+	:text ""))
 (def area2 (text :multi-line? true :font "MONOSPACED-PLAIN-14"
-        :text (text area) :background :black :foreground :white))
+	:text (text area) :background :black :foreground :white))
 (def b (button :text "Run code"))
-(listen b :action (fn [e] (text! area2 (text area)))) ;this should update the text of lb
 (defn display-split [left, right, bottom]
-        (display (top-bottom-split
-                (display (left-right-split (scrollable left) (scrollable right) :divider-location 1/2)) b :divider-location 8/9)))
+	(display (top-bottom-split
+		(display (left-right-split (scrollable left) (scrollable right) :divider-location 1/2)) b :divider-location 8/9)))
 (defn show-frame [] (-> f pack! show!) (display-split area area2 b))
-
-(show-frame)
-
-(defn -main
-	[]
-	(show-frame))
+(defn -main []
+	(native!)
+	(listen b :action (fn [e] (text! area2 (text area)))) ;this should update the text of lb
+	(show-frame)
+)
