7a3459373d289680d7797b8c864b27be963c58c7
Haoxi Zhan <hzcommit@sina.com>
2013-12-07 17:18:27 -0500

    clean the codes

--- src/qgame_seesaw/core.clj
+++ src/qgame_seesaw/core.clj
@@ -3,28 +3,60 @@
   (:use [seesaw.core]
 	[seesaw.chooser]))
 
+;; The whole frame
 (def f (frame :title "QGAME" :width 800 :height 800))
+
 (defn display [content]
+	"A function to display things on screen"
 	(config! f :content content)
 	content)
+
+;; The left-panel which is for inputs
 (def area (text :multi-line? true :font "MONOSPACED-PLAIN-14"
 	:text ""))
+
+;; The right-panel which is for outputs
 (def area2 (text :multi-line? true :font "MONOSPACED-PLAIN-14"
 	:text (text area) :background :black :foreground :white))
-(def b (button :text "Run code"))
-(def exit-b (button :text "Exit"))
+
+;; The "Exit button"
+(def exit-b (button :text "Exit"
+			:listen [:action (fn [e] (System/exit 0))]))
+
+;; The "Number of qubits" inputing field
 (def field (text :multi-line? false :font "MONOSPACED-PLAIN-14" :text""))
+
+;; Running the program
+(defn run-prog
+	[]
+	(let [noq (try (Integer/parseInt (text field)) (catch Exception e (alert "number-of-qubits is supposed to be an Integer.")))]
+		(if (< noq 2)
+			(alert "number-of-qubits too small")
+			(text! area2 (str noq (text area))))))
+
+;; The "About" button
 (def about-button (button :text "About"
 			:listen [:action (fn [e] (alert "About \n Quantum Gate And Measurement Emulator \n 
 			Original author: \n Lee Spector \n
 			Clojure version authors: \n Omri Bernstein \n Evan Ricketts \n Haoxi Zhan \n Breton Handy \n Mitchel Fields"))]))
+			
+;; The "Save result" button
 (def save-area2 (button :text "Save result"
 			:listen [:action (fn [e] (spit (choose-file :type :save :multi? false) (text area2)))]))
+			
+;; The "Save input" button
 (def save-area (button :text "Save input"
 			:listen [:action (fn [e] (spit (choose-file :type :save :multi? false) (text area2)))]))
+			
+;; The "Open" button
 (def open-area (button :text "Open"
 			:listen [:action (fn [e] (text! area (slurp (choose-file :type :open :multi? false))))]))
 
+;; The "Run code" button
+(def b (button :text "Run code"
+		:listen [:action (fn [e] (run-prog))]))
+		
+;; Splitting the whole frame
 (defn display-split []
 	(display (top-bottom-split
 		;top buttons
@@ -50,18 +82,11 @@
 		; the divider-location of top bar and other contents
 		:divider-location 1/20)))
 		
-
-(defn run-prog
-	[]
-	(let [noq (try (Integer/parseInt (text field)) (catch Exception e (alert "number-of-qubits is supposed to be an Integer.")))]
-		(if (< noq 2)
-			(alert "number-of-qubits too small")
-			(text! area2 (str noq (text area))))))
-;
+			
+;; Showing the frames
 (defn show-frame [] (-> f pack! show!) (display-split))
+
 (defn -main [& args]
 	(native!)
-	(listen b :action (fn [e] (run-prog)))
-	(listen exit-b :action (fn [e] (System/exit 0)))
 	(show-frame)
 )
