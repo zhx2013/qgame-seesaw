7ed39c1a54af9af7e87c5ad5db59f31ebec5dcab
Haoxi Zhan <hzcommit@sina.com>
2013-12-07 14:14:28 -0500

    add quit function

--- src/qgame_seesaw/core.clj
+++ src/qgame_seesaw/core.clj
@@ -12,12 +12,15 @@
 (def area2 (text :multi-line? true :font "MONOSPACED-PLAIN-14"
 	:text (text area) :background :black :foreground :white))
 (def b (button :text "Run code"))
-(defn display-split [left, right, bottom]
+(def exit-b (button :text "Exit"))
+(defn display-split []
 	(display (top-bottom-split
-		(display (left-right-split (scrollable left) (scrollable right) :divider-location 1/2)) b :divider-location 8/9)))
-(defn show-frame [] (-> f pack! show!) (display-split area area2 b))
+		(display (left-right-split (scrollable area) (scrollable area2) :divider-location 1/2)) 
+		(display (left-right-split b exit-b :divider-location 1/2)) :divider-location 8/9)))
+(defn show-frame [] (-> f pack! show!) (display-split))
 (defn -main [& args]
 	(native!)
 	(listen b :action (fn [e] (text! area2 (text area))))
+	(listen exit-b :action (fn [e] (System/exit 0)))
 	(show-frame)
 )
