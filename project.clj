05a69eea343a971b04b5743b52e49af0adfa1bd9
Haoxi Zhan <hzcommit@sina.com>
2013-12-08 10:11:16 +0800

    Delete qgame codes and depend on qgame package

--- project.clj
+++ project.clj
@@ -2,6 +2,5 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.5.1"]
 		 [seesaw "1.4.2"]
-		 [net.mikera/core.matrix "0.14.0"]
-		 [org.clojure/math.numeric-tower "0.0.2"]]
+		 [org.clojars.hippiccolo/qgame "0.1.0"]]
   :main qgame-seesaw.core)
